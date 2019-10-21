dd506beab8bac369b3f0e7ecf171318f0ac7e8c5
jcenny <jcenny26@gmail.com>
2019-10-21 16:33:08 -0700

    "updates to getElementsbyClassName"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,21 +9,62 @@ var getElementsByClassName = function(className) {
   var results = [];
 
   var findClass = function (node) {
-    // if has node classlist && node classlist contains className
+
+    // if node has a list && node.classList has className
+    //
     if (node.classList && node.classList.contains(className)) {
+      //push into resultsArray
       results.push(node);
     }
+    // if node has childNodes
     if (node.childNodes) {
       for (var i = 0; i < node.childNodes.length; i++) {
-
-        return findClass(node.childNodes[i]);
-
+        console.log('at iteration ', i);
+        console.log('node.childeNodes is ', node.childNodes);
+        // console.log('node.childnodes.classList is ',node.childnodes.classList);
+        console.log('node.classList is', node.classList);
+        console.log('node.className is', node.className);
+        console.log('node.childNodes[i].nodeName is ', node.childNodes[i].nodeName);
+        console.log('node.childnodes[i] is', node.childNodes[i]);
+        console.log('----------------------');
+        findClass(node.childNodes[i]);
       }
     }
+
   };
 
-
-  results = findClass(document.body);
-  return results;
-
+  // call findClass on document body
+  findClass(document.body);
+return results;
 };
+
+// var htmlStrings = [
+//   '<div class="targetClassName"></div>',
+//   '<div class="otherClassName targetClassName"></div>',
+//   '<div><div class="targetClassName"></div></div>',
+//   '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
+//   '<div><div></div><div><div class="targetClassName"></div></div></div>',
+//   '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
+//   '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+// ];
+
+// describe('getElementsByClassName', function() {
+
+//   it('should match the results of calling the built-in function', function() {
+//     $('body').addClass('targetClassName');
+//     htmlStrings.forEach(function(htmlString) {
+//       var $rootElement = $(htmlString);
+//       $('body').append($rootElement);
+
+//       var result = getElementsByClassName('targetClassName');
+//       var expectedNodeList = document.getElementsByClassName('targetClassName');
+//       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+//       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
+//       expect(equality).to.equal(true);
+
+//       $rootElement.remove();
+//     });
+//     $('body').removeClass('targetClassName');
+//   });
+
+// });
