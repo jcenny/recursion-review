e4dceb55ba6b38bcef98feba4a255d8fbe6ccadc
jcenny <jcenny26@gmail.com>
2019-10-21 11:49:17 -0700

    "stringifyJSON test complete"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    result += obj;
+  } else if (typeof obj === 'string') {
+    result += '"' + obj + '"';
+  } else if (obj === null) {
+    result += 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    for (var i=0; i<obj.length; i++) {
+      result += stringifyJSON(obj[i]) + ',';
+    }
+    result = '[' + result.slice(0,result.length-1) + ']';
+  } else if ((typeof obj === 'object') && (obj !== null)) {
+    for (var key in obj) {
+      if (obj === {} || typeof obj[key] === 'function' || obj[key] === undefined) {
+        return '{}';
+      }
+      result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    result = '{' + result.slice(0,result.length-1) + '}';
+  }
+  return result;
 };
